84f840b2dcba6fc6b48103becfa4d1c199a64051
hassanmohsin <mhassan@miners.utep.edu>
2019-10-07 16:16:21 -0600

    add class

--- get_kinase_pki.py
+++ get_kinase_pki.py
@@ -16,36 +16,47 @@ from scripts import FeatureGenerator
 #from keras.models import load_model
 import pickle
 
-filepath = os.getcwd()
+class pKiPred(object):
+    
+    def __init__(self):
+        self.model = joblib.load('models/Random_forest_gridsearch_py27.mdl')
 
-def get_smi_features(smiles):
-    try:
-        feat_gen = FeatureGenerator(smiles)
-        features = feat_gen.toTPATF()
-        return features
-    except:
+    def get_smi_features(self, smiles):
+        try:
+            feat_gen = FeatureGenerator(smiles)
+            features = feat_gen.toTPATF()
+            return features
+        except:
+            return None
+
+    def get_features(self, seq, smi):
+        p = PyPro()
+        try:
+          p.ReadProteinSequence(seq)
+          features = list(p.GetALL().values())
+      
+          smi_features = self.get_smi_features(smi)
+          smi_features2 = list(np.array([f for f in smi_features], dtype=np.float32))
+      
+          total_features = np.array(features+smi_features2)[np.newaxis, :]
+    #      total_features = np.array(smi_features2+features)[np.newaxis, :] # does not work...!
+          return total_features
+        except Exception as e:
+          print(str(e))
         return None
 
+    def predict(self, seq, smi):
+        protein_feature = self.get_features(seq, smile)
+        return self.model.predict(protein_feature)
 
-def get_features(seq, smi):
-    p = PyPro()
-    try:
-      p.ReadProteinSequence(seq)
-      features = list(p.GetALL().values())
-      
-      smi_features = get_smi_features(smi)
-      smi_features2 = list(np.array([f for f in smi_features], dtype=np.float32))
-      
-      total_features = np.array(features+smi_features2)[np.newaxis, :]
-#      total_features = np.array(smi_features2+features)[np.newaxis, :] # does not work...!
-      return total_features
-    except Exception as e:
-      print(str(e))
-      return None
+
 
 if __name__=='__main__':
-    protein_feature = get_features(sys.argv[1], sys.argv[2])
-    load_model = joblib.load('models/Random_forest_gridsearch_py27.mdl')
-    y_pred = load_model.predict(protein_feature)
-    
-    print(y_pred)
+    seq = "MGCGCSSHPEDDWMENIDVCENCHYPIVPLDGKGTLLIRNGSEVRDPLVTYEGSNPPASPLQDNLVIALHSYEPSHDGDLGFEKGEQLRILEQSGEWWKAQSLTTGQEGFIPFNFVAKANSLEPEPWFFKNLSRKDAERQLLAPGNTHGSFLIRESESTAGSFSLSVRDFDQNQGEVVKHYKIRNLDNGGFYISPRITFPGLHELVRHYTNASDGLCTRLSRPCQTQKPQKPWWEDEWEVPRETLKLVERLGAGQFGEVWMGYYNGHTKVAVKSLKQGSMSPDAFLAEANLMKQLQHQRLVRLYAVVTQEPIYIITEYMENGSLVDFLKTPSGIKLTINKLLDMAAQIAEGMAFIEERNYIHRDLRAANILVSDTLSCKIADFGLARLIEDNEYTAREGAKFPIKWTAPEAINYGTFTIKSDVWSFGILLTEIVTHGRIPYPGMTNPEVIQNLERGYRMVRPDNCPEELYQLMRLCWKERPEDRPTFDYLRSVLEDFFTATEGQYQPQP"
+    smile = "CC(C)Oc1ccc(cc1Cl)c2noc(n2)c3ccc(N[C@H]4CC[C@H](C4)C(=O)O)cc3"
+    pkipred = pKiPred()
+
+    if len(sys.argv) == 1:
+        print(pkipred.predict(seq, smile))
+    else:
+        print(pkipred.predict(sys.argv[1], sys.argv[2]))
