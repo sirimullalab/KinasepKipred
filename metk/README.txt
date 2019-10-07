8b742800d2b7b93080848dd96d11a569a21eceff
Govinda KC <kcgovindap@gmail.com>
2019-10-06 21:04:59 -0600

    files removed

--- metk/README.txt
+++
@@ -1,1 +0,0 @@
- Intially this code was written for confidence interval interms of R^2 but i changed it for only R or rho.
--- metk/modelevaltoolkit/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Pat Walters'
-__email__ = 'Please add email'
-__version__ = '0.1.0'
