f07d733fa2315d4e79b5784809fa31303205058d
Mahmudulla Hassan <mhassan@miners.utep.edu>
2019-11-22 19:44:44 -0700

    Add script for downloading the model

---
+++ download_model.sh
@@ -0,0 +1,4 @@
+curl -O https://gitlab.com/hassanmohsin/kinasepkipred/raw/master/Random_forest_gridsearch_py27.tar.gz
+tar -xvf Random_forest_gridsearch_py27.tar.gz
+rm -rf Random_forest_gridsearch_py27.tar.gz
+
